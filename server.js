8ed8797475c6211724952b79d6f2690ad483b951
Nathalia Patricia <nathaliapatricia0805@gmail.com>
2026-01-11 11:21:05 -0300

    Encutador LUMION

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const path = require('path');
 const express = require('express');
-const cors = require('cors'); // Certifique-se de que o cors está instalado no package.json
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const links = {};
 
 app.use(express.json());
 app.use(cors());
-
-// Serve arquivos estáticos da raiz e da pasta assets
-app.use(express.static(path.join(__dirname))); 
+app.use(express.static(__dirname));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
@@ -21,19 +19,28 @@ const generateCode = len => {
     do {
         code += (Math.random().toString(36).substring(2))
     } while (code.length < len);
-
     return code.substring(0, len);
 };
 
-console.log("Codigo gerado: ", generateCode(6));
-console.log("Outro codigo: ", generateCode(6));
+app.post('/shorten', (req, res) => {
+    const url = req.body.url;
+    const customCode = req.body.customCode;
+    const code = (customCode && customCode.trim() !== '') ? customCode : generateCode(6);
+
+    if (links[code]) {
+        return res.status(400).json({ error: 'Esse código já está em uso! Escolha outro.' });
+    }
+
+    links[code] = url;
+    res.json({ shortCode: code, originalUrl: url});
+});
 
 app.get('/:shortCode', (req, res) => { 
     const shortCode = req.params.shortCode;
 
-    // SE TIVER PONTO (ex: .css, .png), É UM ARQUIVO, NÃO UM LINK. PARE AQUI.
+    // Ignora requisições de arquivos
     if (shortCode.includes('.')) {
-        return next(); 
+        return;
     }
 
     const urlOriginal = links[shortCode];
@@ -43,21 +50,8 @@ app.get('/:shortCode', (req, res) => {
     res.redirect(urlOriginal); 
 });
 
-app.post('/shorten', (req, res) => {
-    const url = req.body.url;
-    const customCode = req.body.customCode
-    const code = (customCode && customCode.trim() !== '') ? customCode : generateCode(6);
-
-    if (links[code]) {
-        return res.status(400).json({ error: 'Esse código já está em uso! Escolha outro.' });
-    }
-
-    links[code] = url;
-    res.json({ shortCode: code, originalUrl:url});
-});
-
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
